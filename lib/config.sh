a575a3166e9133425e6954da826f63a7211d46db
uMa <ysmst35@gmail.com>
2022-06-05 18:50:14 +0900

    vimrcを追加

--- lib/config.sh
+++ lib/config.sh
@@ -1,4 +1,5 @@
 INSTALL_CONFIGS=(
   "<file>zshrc</file>       <path>$HOME/.zshrc</path>"
   "<file>zplug.zshrc</file> <path>$HOME/.zplug.zshrc</path>"
+  "<file>vimrc</file>       <path>$HOME/.vimrc</path>"
 )
